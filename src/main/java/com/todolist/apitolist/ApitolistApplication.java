a15bc1fa02d032e6565721291131406fde6993e8
DanielCoelhoCDK <danielcoelho.cdk@gmail.com>
2023-10-12 15:14:47 -0300

    subindo o projeto para o repo remoto

---
+++ src/main/java/com/todolist/apitolist/ApitolistApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.apitolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/apitolist/ApitolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.apitolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
